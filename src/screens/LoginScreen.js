a5a4d0a539b02fc64836e36db65d39a18f91c525
varekai-dev <serhijsav@gmail.com>
2021-02-14 10:04:34 +0200

    fix logo

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -9,7 +9,7 @@ function LoginScreen() {
       <div className="loginScreen__background">
         <img
           className="loginScreen__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="https://logos-download.com/wp-content/uploads/2016/03/Netflix_logo.png"
           alt="netflix"
         />
         <button
